6c5a7adcb83de19ca70138c9f299be793ae17a3e
Danur Arya <danuraryapratama@gmail.com>
2024-04-03 08:23:37 +0700

    initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("API/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly StoreContext _context;
+    public ProductsController(StoreContext context)
+    {
+        _context = context;
+
+    }
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts() => 
+        Ok(await _context.Products.ToListAsync());
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id) =>
+        Ok(await _context.Products.FindAsync(id));
+
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class StoreContext(DbContextOptions<StoreContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().HasData(
+            new Product {Id = 1, Name = "Product 1"},
+            new Product {Id = 2, Name = "Product 2"},
+            new Product {Id = 3, Name = "Product 3"}
+        );
+    }
+}
